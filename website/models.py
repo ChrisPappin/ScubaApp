c1fb780f8f22323a0be93a200cfb50767ccf3c2d
Chris Pappin <pappinc@mcmaster.ca>
2022-05-19 15:56:14 -0400

    Changed Database relationships

--- website/models.py
+++ website/models.py
@@ -2,12 +2,11 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Summary(db.Model):
-    id = db.Columnn(db.Integer, primary_key=True)
-    totalDives = db.Column(db.String(150))
-    uniqueLoc = db.Column(db.String(150))
-    totalTime = db.Column(db.String(150))
-    
+#class Summary(db.Model):
+    #userID = db.Column(db.Integer, db.ForeignKey('user.id')) #One to one relationship, needed?
+    #totalDives = db.Column(db.String(150))
+    #uniqueLoc = db.Column(db.String(150))
+    #totalTime = db.Column(db.String(150))
 
     #Auto add datetime:
     #date = db.Column(db.DateTime(timezone=True), default=)
@@ -17,3 +16,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
+    #summary = db.relationship('Summary')
+    totalDives = db.Column(db.String(150))
+    uniqueLoc = db.Column(db.String(150))
+    totalTime = db.Column(db.String(150))
